679b00f5fc27ca63a7ad1cadbe79b1f6a06c3f96
alfred <alfredng019@gmail.com>
2026-07-26 12:03:25 -0400

    switched to unstyled for custom styling

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,16 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@mantine/core/styles.css'
 import '@mantine/notifications/styles.css'
-import { MantineProvider } from '@mantine/core'
+import { HeadlessMantineProvider } from '@mantine/core'
 import { Notifications } from '@mantine/notifications'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MantineProvider defaultColorScheme="auto">
+    <HeadlessMantineProvider>
       <Notifications position="top-right" />
       <App />
-    </MantineProvider>
+    </HeadlessMantineProvider>
   </StrictMode>,
 )
